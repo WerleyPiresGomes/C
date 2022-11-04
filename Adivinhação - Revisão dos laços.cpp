eebcb6a63e0b4ad49f12044f5f221265525af4b2
WERLEY PIRES GOMES <114071524+WerleyPiresGomes@users.noreply.github.com>
2022-11-04 08:40:40 -0300

    Add files via upload

--- Adivinhação - Revisão dos laços.cpp
+++ Adivinhação - Revisão dos laços.cpp
@@ -4,22 +4,27 @@ int main()
 {	
 	int num;
 	int num2;
+	int count;
 	num=10;
-	printf("********************\n");
-	printf("* DIGITE UM NUMERO * \n");
-	printf("********************\n");
-	scanf ("%d",&num2);
-	if (num2==num)
+	for(count=0; count<3; count++)
 	{
-		printf (" PARABENS VOCE ACERTOU =  %d",num2);
-	}
-	if (num2>num)
-	{
-		printf (" Voce Errou \n O Numero digitado %d e maior",num2);
-	}
-	if (num2<num)
-	{
-		printf (" Voce Errou \n O Numero digitado %d e menor",num2);
+		printf("\n********************\n");
+		printf("* DIGITE UM NUMERO * \n");
+		printf("********************\n");
+		scanf ("%d",&num2);
+		if (num2==num)
+		{
+			printf (" PARABENS VOCE ACERTOU =  %d\n",num2);
+			count =3;
+		}
+		if (num2>num)
+		{
+			printf (" Voce Errou \n O Numero digitado %d e maior\n",num2);
+		}
+		if (num2<num)
+		{
+			printf (" Voce Errou \n O Numero digitado %d e menor\n",num2);
+		}
 	}
 
 }
