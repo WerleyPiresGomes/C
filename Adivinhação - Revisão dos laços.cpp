bf296b79b648f5f9b85fe91fa87e231053b03a37
WERLEY PIRES GOMES <114071524+WerleyPiresGomes@users.noreply.github.com>
2022-11-04 08:17:42 -0300

    Add files via upload

--- Adivinhação - Revisão dos laços.cpp
+++ Adivinhação - Revisão dos laços.cpp
@@ -2,12 +2,24 @@
 #include <conio.h>
 int main()
 {	
-int num;
-int num2;
-num=10;
-printf("DIGITE UM NUMERO \n");
-scanf ("%d",&num2);
-printf (" O NUMERO DIGITADO =  %d",num);
-printf (" O NUMERO DIGITADO =  %d",num2);
+	int num;
+	int num2;
+	num=10;
+	printf("********************\n");
+	printf("* DIGITE UM NUMERO * \n");
+	printf("********************\n");
+	scanf ("%d",&num2);
+	if (num2==num)
+	{
+		printf (" PARABENS VOCE ACERTOU =  %d",num2);
+	}
+	if (num2>num)
+	{
+		printf (" Voce Errou \n O Numero digitado %d e maior",num2);
+	}
+	if (num2<num)
+	{
+		printf (" Voce Errou \n O Numero digitado %d e menor",num2);
+	}
 
 }
