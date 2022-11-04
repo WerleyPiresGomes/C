0f38fa29c6689030c9d5db305da4e0841fbd9925
WERLEY PIRES GOMES <114071524+WerleyPiresGomes@users.noreply.github.com>
2022-11-04 08:04:22 -0300

    Add files via upload

---
+++ Adivinhação - Revisão dos laços.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{	
+int num;
+int num2;
+num=10;
+printf("DIGITE UM NUMERO \n");
+scanf ("%d",&num2);
+printf (" O NUMERO DIGITADO =  %d",num);
+printf (" O NUMERO DIGITADO =  %d",num2);
+
+}
