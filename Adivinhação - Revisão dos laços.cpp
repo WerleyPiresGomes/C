d63d5ebeb16723948b933a1282c7309baaf921b3
WERLEY PIRES GOMES <114071524+WerleyPiresGomes@users.noreply.github.com>
2022-11-07 08:02:33 -0300

    Add files via upload

--- Adivinhação - Revisão dos laços.cpp
+++ Adivinhação - Revisão dos laços.cpp
@@ -1,3 +1,9 @@
+/*	Name: Revisao logica de programacao C
+	Copyright: 
+	Author: WERLEY PIRES GOMES
+	Date: 07/11/22 008:02
+	Description: PROGRAMA, ADIVINHACAO DE NUMERO USO DO while 
+*/
 #include <stdio.h>
 #include <conio.h>
 int main()
@@ -5,26 +11,35 @@ int main()
 	int num;
 	int num2;
 	int count;
+	int repete;
 	num=10;
-	for(count=0; count<3; count++)
+	repete=1;
+	while (repete==1)
 	{
-		printf("\n********************\n");
-		printf("* DIGITE UM NUMERO * \n");
-		printf("********************\n");
-		scanf ("%d",&num2);
-		if (num2==num)
+		
+		
+		for(count=0; count<3; count++)
 		{
-			printf (" PARABENS VOCE ACERTOU =  %d\n",num2);
-			count =3;
+			printf("\n************************************\n");		
+			printf("* DIGITE UM NUMERO, TETATIVA %d DE 3 * \n",count+1);
+			printf("************************************\n");	
+			scanf ("%d",&num2);
+			if (num2==num)
+			{
+				printf (" PARABENS VOCE ACERTOU =  %d\n",num2);
+				count =3;
+			}
+			if (num2>num)
+			{
+				printf (" Voce Errou \n O Numero digitado %d e maior\n",num2);
+			}
+			if (num2<num)
+			{
+				printf (" Voce Errou \n O Numero digitado %d e menor\n",num2);
+			}
 		}
-		if (num2>num)
-		{
-			printf (" Voce Errou \n O Numero digitado %d e maior\n",num2);
-		}
-		if (num2<num)
-		{
-			printf (" Voce Errou \n O Numero digitado %d e menor\n",num2);
-		}
+		printf (" \n ** GAME OVER ** ");
+		printf (" \n ** Jogar novamente digite  0=NAO  1=SIM ** ");
+		scanf ("%d",&repete);
 	}
-
 }
